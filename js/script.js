3dc9f0bf084befd49158290e837f408f9b8631e2
MD Shimul <mdshemul480@gmail.com>
2021-09-06 18:17:39 +0600

    adding delete todo list item

--- js/script.js
+++ js/script.js
@@ -2,17 +2,27 @@ const inputBox = document.getElementById("inputBox");
 const inputButton = document.getElementById("inputButton");
 const allTodos = document.getElementById("all-todo-list");
 
-const todoList = [];
+let todoList = [];
+
+const deleteTodo = (element) => {
+  const item = element.target;
+  const itemId = +item.getAttribute("data-list-index");
+  todoList = todoList.filter((_, index) => index !== itemId);
+  console.log(todoList);
+  showResult();
+};
 
 const showResult = () => {
   allTodos.textContent = "";
-  const listElements = document.createElement("ul");
-  todoList.forEach((item) => {
+  const listElements = document.createElement("ol");
+  todoList.forEach((item, index) => {
     const listElement = document.createElement("li");
     listElement.innerText = item;
+    listElement.setAttribute("data-list-index", index);
     listElements.appendChild(listElement);
   });
   allTodos.appendChild(listElements);
+  listElements.addEventListener("click", deleteTodo);
 };
 
 inputButton.addEventListener("click", () => {
